97dc58c667ca557db197e366f5bf2cc95e9f8f3c
Koby Lewis <kobyjlewis@gmail.com>
2020-05-27 23:07:45 -0600

    last commit wasn't what I meant, this actually includes the changes

--- Conlang/Phoneme.cpp
+++ Conlang/Phoneme.cpp
@@ -22,7 +22,7 @@ bool Phoneme::includesTypes(initializer_list<PhonemeTypes> types) const {
 map<string, PhonemeTypes> Phoneme::stringToTypeMap = { 
 	{"consonant", CONSONANT}, {"pulmonic", PULMONIC}, {"voiced", VOICED}, {"unvoiced", UNVOICED},
 	{"bilabial", BILABIAL}, {"labiodental", LABIODENTAL}, {"dental", DENTAL}, {"alveolar", ALVEOLAR}, {"postalveolar", POSTALVEOLAR}, {"retroflex", RETROFLEX}, {"alveolopalatal", ALVEOLOPALATAL}, {"palatal", PALATAL}, {"velar", VELAR}, {"uvular", UVULAR}, {"pharyngeal", PHARYNGEAL}, {"glottal", GLOTTAL},
-	{"plosive", PLOSIVE}, {"sibilant", SIBILANT}, {"nonsibilant-fricative", NONSIBILANT_FRICATIVE}, {"nasal", NASAL}, {"approximant", APPROXIMANT}, {"tap", TAP}, {"trill", TRILL}, {"lateral-fricative", LATERAL_FRICATIVE}, {"lateral-approximant", LATERAL_APPROXIMANT}, {"lateral-flap", LATERAL_FLAP},
+	{"plosive", PLOSIVE}, {"sibilant", SIBILANT}, {"nonsibilant", NONSIBILANT}, {"fricative", FRICATIVE}, {"nasal", NASAL}, {"approximant", APPROXIMANT}, {"tap", TAP}, {"trill", TRILL}, {"lateral-fricative", LATERAL_FRICATIVE}, {"lateral-approximant", LATERAL_APPROXIMANT}, {"lateral-tap", LATERAL_TAP},
 	{"affricate", AFFRICATE}, {"coarticulated", COARTICULATED},
 	{"nonpulmonic", NONPULMONIC},{"click", CLICK},{"implosive", IMPLOSIVE},{"ejective", EJECTIVE},
 	{"vowel", VOWEL},{"rounded", ROUNDED},{"unrounded", UNROUNDED},
